21b9558ab377f845548cf5927b08c49394f3ffe0
Valentine McBean-Willis <valentine.mcbean-willis@afrilogicsolutions.com>
2025-08-13 18:11:14 +0100

    changes

--- src/components/normal-side/about-page.jsx
+++ src/components/normal-side/about-page.jsx
@@ -4,19 +4,29 @@ import Footer from '../layout/footer'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFlag,faCheckCircle} from "@fortawesome/free-regular-svg-icons";
 import { faSearchLocation } from "@fortawesome/free-solid-svg-icons";
-import AboutImage from '../../assets/images/all-green.jpg'
+import AboutImage from '../../assets/images/people-on-grass.jpg'
+import MissionImage from '../../assets/images/green-buildings.webp'
 
  export default function AboutPage() {
     return (
 <>
 <LandingHeader />
 <Main className="p-4 flex-1">
-<div className="flex flex-col min-w-full min-h-full justify-center px-0 md:min-w-3/5 lg:min-w-3/4 sm:px-16 md:px-32 my-8">
-            <h2 className="flex items-center font-semibold text-white  text-3xl my-4 bg-no-repeat  w-full bg-center min-h-100 p-4 rounded-xl" style={{ backgroundImage: `url(${AboutImage})`,  backgroundSize: "cover", }}>About Omni City</h2>
-            <h3 className="font-medium text-2xl mb-2 mt-4 px-4">Our Mission</h3>
-            <p className="mb-4 px-4">
+<div className="flex flex-col min-w-full min-h-full justify-center px-0 my-8">
+            <h2 className="flex items-center font-bold text-white  text-4xl my-4 bg-no-repeat  w-full bg-center min-h-100 p-4 rounded-xl" style={{ backgroundImage: `url(${AboutImage})`,  backgroundSize: "cover", }}>About Omni City</h2>
+                <div className="flex w-full h-full mt-16 gap-4 sm:bg-black/60 rounded-xl px-8 py-24 relative">
+                    <img src={MissionImage} alt="" className="flex min-w-full min-h-full rounded-xl sm:w-1/2" />
+                    <div className="w-full min-h-full flex flex-col justify-center gap-4 bg-black/60 sm:bg-black/1 sm:w-1/2 sm:static absolute ml-auto">
+            <h3 className="font-bold text-4xl text-white w-full">Our Mission</h3>
+            <p className="flex mb-4 text-white lg:text-2xl w-full justify-between">
                 Smart City Platform is dedicated to enhancing community living by providing a streamlined platform for reporting and resolving public property maintenance issues. Our mission is to empower citizens to actively participate in maintaining their neighborhoods, ensuring a safe and well-kept environment for everyone.
             </p>
+
+                    </div>
+                    
+
+                </div>
+            
             <h3 className="font-medium text-2xl mb-2 mt-4 px-4">How It Works</h3>
             <p className="mb-4 px-4">
                 <ul className="flex flex-col min-h-1/3 my-4 justify-between list-none gap-8">
